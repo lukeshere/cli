cfe565c91ec37edb27236cb0e14cbe6859eaab1a
luke <lujia.guo@mosesceo.com>
2025-08-22 15:20:56 +0800

    fix

--- src/cli.zig
+++ src/cli.zig
@@ -179,7 +179,8 @@ pub const Spinner = struct {
 
     pub fn init(message: []const u8) !Spinner {
         return Spinner{
-            .frames = &[_][]const u8{ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+            // .frames = &[_][]const u8{ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+            .frames = &[_][]const u8{ "|", "/", "/", "|", "\\", "\\", "|"},
             .message = message,
             .timer = try std.time.Timer.start(),
             .current = 0,
@@ -193,6 +194,6 @@ pub const Spinner = struct {
 
     pub fn stop(self: *Spinner, message: []const u8) !void {
         _ = self;
-        _ = print("\r✓ {s}\n", .{message});
+        _ = print("\r* {s}\n", .{message});
     }
 };
